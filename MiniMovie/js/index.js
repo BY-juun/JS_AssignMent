db4f36054642d5b381aaeb64de02bc915a633465
BY-juun <neostgeart@gmail.com>
2022-05-11 11:43:44 +0900

    async -> Promise

--- MiniMovie/js/index.js
+++ MiniMovie/js/index.js
@@ -3,18 +3,45 @@ import render from "./render.js";
 
 export let data = null;
 
-const fetchMovieData = async () => {
-	data = (await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=38447fbd2f00156efebbd1edbb57835f`)).json()).results;
-	data = data.map((value) => {
-		return {
-			...value,
-			like: false,
-		}
+// const fetchMovieData = async () => {
+// 	data = (await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=38447fbd2f00156efebbd1edbb57835f`)).json()).results;
+// 	data = data.map((value) => {
+// 		return {
+// 			...value,
+// 			like: false,
+// 		}
+// 	})
+// 	makeCards(data);
+// };
+
+const fetchMovieData = () => {
+	return new Promise((resolve, reject) => {
+		resolve(customFetch(`https://api.themoviedb.org/3/movie/popular?api_key=38447fbd2f00156efebbd1edbb57835f`));
 	})
-	makeCards(data);
-};
+}
+
+const customFetch = (url) => {
+	return fetch(url)
+		.then((res) => {
+			if (res.ok === true)
+				return res.json();
+			else
+				throw new Error("서버 에러");
+		})
+		.then((res) => {
+			data = res.results;
+			return (res.results);
+		})
+		.catch((error) => {
+			alert(error);
+		})
+
+}
+
 window.addEventListener('popstate', () => {
 	render()
 })
 
-fetchMovieData();
+fetchMovieData().then((res) => {
+	makeCards(res);
+});
